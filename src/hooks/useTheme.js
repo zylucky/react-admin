6f0f6529f585b83368e28419c3552c0e1d8f61c7
zhangyuan1 <zhangyuan1@cqslim.com>
2023-10-10 18:25:50 +0800

    切换主题颜色修复

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -5,7 +5,7 @@ import "@/styles/theme/theme-dark.less";
  * @description 全局主题设置
  * */
 const useTheme = themeConfig => {
-	const { weakOrGray } = themeConfig;
+	const { weakOrGray, isDark } = themeConfig;
 
 	const initTheme = () => {
 		// 灰色和弱色切换
@@ -15,6 +15,19 @@ const useTheme = themeConfig => {
 		if (weakOrGray === "gray") body.setAttribute("style", "filter: grayscale(1)");
 
 		// 切换暗黑模式
+		let head = document.getElementsByTagName("head")[0];
+		const getStyle = head.getElementsByTagName("style");
+		if (getStyle.length > 0) {
+			for (let i = 0, l = getStyle.length; i < l; i++) {
+				if (getStyle[i]?.getAttribute("data-type") === "dark") getStyle[i].remove();
+			}
+		}
+		let styleDom = document.createElement("style");
+		styleDom.dataset.type = "dark";
+		const darkThemeStyle = "body {background-color: #141414 !important;color: rgba(255, 255, 255, .85);}";
+		const defaultThemeStyle = "body {background-color: #ffffff !important;color: rgba(0, 0, 0, .85);}";
+		styleDom.innerHTML = isDark ? darkThemeStyle : defaultThemeStyle;
+		head.appendChild(styleDom);
 	};
 	initTheme();
 
